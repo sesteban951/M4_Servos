6938888ccb56af862c7d9eaf80d3092cd813cd08
sesteban951 <sesteban@caltech.edu>
2025-07-12 16:27:33 -0700

    cleaned up the code on both sides

--- commands/src/main.cpp
+++ commands/src/main.cpp
@@ -1,3 +1,13 @@
+/*
+    Simple script to control servos over Serial communication.
+    WARNING: 1) Do not communicate over Serial while actively uploading to Arduino.
+             2) Use "ls /dev/ttyACM*" to find the correct port. This is different on Windows (COM*).
+
+    Servo details:
+    - Savox SV1261MG - Mini Digital High Voltage Aluminum Case Servo 0.095/277@ 7.4V
+    - https://www.savoxusa.com/products/sv1261mg-mini-digital-high-voltage-aluminum-case-servo-0-095-277-7-4v#technical-details
+*/
+
 // srandard includes
 #include <iostream>
 
@@ -10,18 +20,16 @@
 // main function
 int main() {
 
-    // open serial port
-    // Replace with your Teensy's port (e.g., 'COM3' on Windows or '/dev/ttyACM0' on Linux)
-    // NOTE: use "ls /dev/ttyACM*" to find the correct port
-    int serial_port = open("/dev/ttyACM0", O_RDWR);
-    if (serial_port < 0) {
-        std::cerr << "Failed to open serial port!\n";
+    // open  Teensy serial port
+    int teensy_serial_port = open("/dev/ttyACM0", O_RDWR); // NOTE: (2)
+    if (teensy_serial_port < 0) {
+        std::cerr << "Failed to open Teensy serial port!\n";
         return 1;
     }
 
     // configure serial port settings
     struct termios tty;
-    tcgetattr(serial_port, &tty);
+    tcgetattr(teensy_serial_port, &tty);
 
     // set baud rate
     cfsetispeed(&tty, B9600);
@@ -35,21 +43,21 @@ int main() {
     tty.c_cflag |= CREAD | CLOCAL;
 
     // apply settings
-    tcsetattr(serial_port, TCSANOW, &tty);
+    tcsetattr(teensy_serial_port, TCSANOW, &tty);
 
     // beginning of communication loop
     while (true) {
-        std::cout << "Enter 1 (on), 0 (off), or q to quit: ";
+        std::cout << "Enter 1 (ON), 0 (OFF), or q to quit: ";
         std::string input;
         std::cin >> input;
         if (input == "q") break;
         if (input == "1" || input == "0") {
-            write(serial_port, input.c_str(), 1);
+            write(teensy_serial_port, input.c_str(), 1);
         }
     }
 
-    std::cout << "Closing serial port...\n";
+    std::cout << "Closing Teensy serial port...\n";
 
-    close(serial_port);
+    close(teensy_serial_port);
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 /*
-
     Simple Arduino sketch to control servos over Serial communication.
-    WARNING: do not upload this sketch while actively communicating over Serial.
+    WARNING: 1) Do not upload this sketch while actively communicating over Serial.
+             2) Make sure to set the correct serial port in the parent program.
+             3) You cannot open the Serial Monitor in Arduino IDE while this sketch is running
+                since it uses the same serial bus.
 
+    Servo details:
+    - Savox SV1261MG - Mini Digital High Voltage Aluminum Case Servo 0.095/277@ 7.4V
+    - https://www.savoxusa.com/products/sv1261mg-mini-digital-high-voltage-aluminum-case-servo-0-095-277-7-4v#technical-details
 */
 
-// standrad arduino inlucdes
+// standard inlucdes
 #include <Arduino.h>
 
-// standard servo 
+// standard servo library
 #include <Servo.h>
+
+// servo pins on Teensy 4.0
+// WARNING: Do not change pins this as they are permanently soldered. 
+//          Same thing with baud rate.
+#define SERVO1_PIN 14
+#define SERVO2_PIN 15
+#define SERVO3_PIN 18
+#define SERVO4_PIN 19
+#define BAUD_RATE 9600
 
 // servo objects
 Servo servo1;
@@ -17,65 +31,74 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 
-// sine wave parameters
-double sin_value = 0.0; // variable to store the sine value
-double frequency = 0.5; // frequency of the sine wave
-int pos = 0;            // variable to store the servo position
+// servo ocontrol parameters
+int armed = 0;              // armed state, 0 = not armed, 1 = armed
+const double deg_pos_0 = 45.0;  // [deg] position when "OFF" (0)
+const double deg_pos_1 = 90.0;  // [deg] position when "ON" (1)
+const double pwm_pos_0 = 800.0;   // [us] position when "OFF" (0)
+const double pwm_pos_1 = 2200.0;  // [us] position when "ON" (1)
 
 void setup()
 {
-    // begin the serial communication over USB
-    Serial.begin(9600);
+    // begin the serial communication over USB. This is the USB cable connected to the computer.
+    Serial.begin(BAUD_RATE);
 
-    // attach the servo
-    servo1.attach(14); 
-    servo2.attach(15); 
-    servo3.attach(18); 
-    servo4.attach(19); 
+    // Attach the servo pins to the Teensy
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN);
+    servo3.attach(SERVO3_PIN);
+    servo4.attach(SERVO4_PIN);
 
-    // turn on LED to indicate ON
+    // builtinLED to indicate status
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop()
 {
-    // calculate the sine value
-    sin_value = sin(2 * 3.14159  * frequency * millis() / 1000.0); // calculate sine value based on time
-    pos = map(sin_value, -1, 1, 0, 180); // map the sine value to servo position (0 to 180 degrees)
-
-    // write the position to the servo
-    servo1.write(pos);
-    servo2.write(pos);
-    servo3.write(pos);
-    servo4.write(pos);
-
     // check if there is data available on the serial port
     if (Serial.available()) {
         
         // read the command from serial
         char cmd = Serial.read();
+        armed = (cmd == '1') ? 1 : 0;
 
         // check the command and control the servos
-        if (cmd == '1') {
-            digitalWrite(LED_BUILTIN, HIGH);
-            // write the position to the servo
-            servo1.write(pos);
-            servo2.write(pos);
-            servo3.write(pos);
-            servo4.write(pos);
-        } 
-        
-        else if (cmd == '0') {
+        if (armed == 0) {
+
+            // set LED indicator to OFF when disarmed
             digitalWrite(LED_BUILTIN, LOW);
-            servo1.write(0);
-            servo2.write(0);
-            servo3.write(0);
-            servo4.write(0);
+            
+            // write the position to the servo
+            // servo1.write(deg_pos_0);
+            // servo2.write(deg_pos_0);
+            // servo3.write(deg_pos_0);
+            // servo4.write(deg_pos_0);
+            servo1.writeMicroseconds(pwm_pos_0);
+            servo2.writeMicroseconds(pwm_pos_0);
+            servo3.writeMicroseconds(pwm_pos_0);
+            servo4.writeMicroseconds(pwm_pos_0);
+        } 
+        else if (armed == 1) {
+
+            // set LED indicator to ON when armed
+            digitalWrite(LED_BUILTIN, HIGH);
+            
+            // write the position to the servo
+            // servo1.write(deg_pos_1);
+            // servo2.write(deg_pos_1);
+            // servo3.write(deg_pos_1);
+            // servo4.write(deg_pos_1);
+            servo1.writeMicroseconds(pwm_pos_1);
+            servo2.writeMicroseconds(pwm_pos_1);
+            servo3.writeMicroseconds(pwm_pos_1);
+            servo4.writeMicroseconds(pwm_pos_1);
+        }
+        else {
+            // unknown command, stay at previous state
         }
     }
 
     // delay to avoid flooding the serial port
-    delay(500);
+    delay(10);
 }
 
